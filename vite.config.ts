376c521f34c250f2eebb229fa85196677b62ca1b
Juan <heguilenjuan@gmail.com>
2025-04-06 17:32:22 -0300

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/',
+  base:'/b6-front-juan/',
   plugins: [react()],
   assetsInclude:["**/*.PNG"],
   resolve:{
