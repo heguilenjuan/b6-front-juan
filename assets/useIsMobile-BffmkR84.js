6161ffeb8572a2ea74f8d454a09db72a04825643
Juan <heguilenjuan@gmail.com>
2025-04-11 19:51:04 -0300

    Updates

---
+++ assets/useIsMobile-BffmkR84.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-f_E2TThQ.js";const o=(e=768)=>{const[i,n]=t.useState(window.innerWidth<e);return t.useEffect(()=>{const s=()=>n(window.innerWidth<e);return window.addEventListener("resize",s),()=>window.removeEventListener("resize",s)},[e]),i};export{o as u};
